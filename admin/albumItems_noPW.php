401b3605c0f45a223bc966188a2e7285e9a2e6c3
Jeremy Butler <jgbutler@users.noreply.github.com>
2023-06-21 15:10:49 -0500

    Buggy code
    The admin section of Shot Logger is very buggy at this point and is not going to be debugged by me. Please make as much use of them as you wish.

--- admin/albumItems_noPW.php
+++ admin/albumItems_noPW.php
@@ -21,13 +21,6 @@
 
 require_once('../Connections/ShotLoggerVM.php'); 
 
-// Check to see if user is logged in.
-
-if (isset($_COOKIE['PrivatePageLogin'])) {
-   if ($_COOKIE['PrivatePageLogin'] == md5($password.$nonsense)) {
-
-// Insert content below.
-
 ?>
 
 <!-- BEGIN PASSWORD-PROTECTED CONTENT HERE -->
@@ -80,15 +73,13 @@ $slDir = $_POST['slDir'] ;
 $slDirPrefix = '/srv/www/shotlogger/images/' ;
 $slTitleID =  $row_rsSlTitle['MAX(slTitleID)'] + 1;
 ?>
+
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 <title>Shot Logger 2:  Item Data</title>
 <link href="admin.css" rel="stylesheet" type="text/css" />
-<!--<link href="https://maxcdn.bootstrapcdn.com/bootswatch/3.3.5/yeti/bootstrap.min.css" rel="stylesheet" integrity="sha256-gJ9rCvTS5xodBImuaUYf1WfbdDKq54HCPz9wk8spvGs= sha512-weqt+X3kGDDAW9V32W7bWc6aSNCMGNQsdOpfJJz/qD/Yhp+kNeR+YyvvWojJ+afETB31L0C4eO0pcygxfTgjgw==" crossorigin="anonymous">-->
-
-
 </head>
 <!-- Spry assets are left over from Dreamweaver coding that relied on them. 
 They should probably be replaced by Jquery form validation. -->
@@ -216,63 +207,6 @@ mysql_free_result($rsSlTitle);
 
 
 <!-- END PASSWORD-PROTECTED CONTENT -->
-
-<?php
-
-// Resume checking to see if user is logged in.
-
-   exit;
-   } else {
-      echo "Bad cookie.";
-      exit;
-   }
-}
-
-if (isset($_GET['p']) && $_GET['p'] == "login") {
-	echo "<!doctype html>
-		<html>
-		<head>
-		<meta charset=\"utf-8\">
-		<link href=\"../shotloggerV2_datatable.css\" rel=\"stylesheet\" type=\"text/css\" />
-		<title>Shot Logger: Login Problem</title>
-		<h1>Shot Logger: Login Problem</h1>";
-   if ($_POST['user'] != $username) {
-      echo "<strong>Sorry. I don't recognize that username.</strong><br />
-		<h2>Return to the previous page to try again.</h2>";
-      exit;
-   } else if ($_POST['keypass'] != $password) {
-      echo "<strong>Sorry. Invalid password.</strong>
-		<h2>Return to the previous page to try again.</h2>";
-      exit;
-   } else if ($_POST['user'] == $username && $_POST['keypass'] == $password) {
-      setcookie('PrivatePageLogin', md5($_POST['keypass'].$nonsense));
-      header("Location: $_SERVER[PHP_SELF]");
-   } else {
-      echo "<strong>Sorry. You could not be logged in at this time.</strong>";
-   }
-}
-?>
-<!doctype html>
-<html>
-<head>
-<meta charset="utf-8">
-<title>Shot Logger: Login</title>
-<link href="admin.css" rel="stylesheet" type="text/css" />
-</head>
-<body>
-<h1>Shot Logger: Administrator Login</h1>
-
-<!-- Begin password form -->
-
-<form action="<?php echo $_SERVER['PHP_SELF']; ?>?p=login" method="post">
-<label><strong>Username:</strong> <br />
-<input type="text" name="user" id="user" /></label><br />
-<label><strong>Password:</strong> <br />
-<input type="password" name="keypass" id="keypass" /></label>
-<p><input type="submit" id="submit" value="Login" /></p>
-</form>
-
-<!-- End password form -->
 
 <?php 
 include ('../includes/footerV2.php'); // Include the footer
--- admin/index.php
+++ admin/index.php
@@ -1,109 +1,55 @@
 <?php 
-/*
-    Shot Logger facilitates the analysis of visual style in film and television 
-	through screen shots and editing statistics.
-    Copyright (C) 2007-2015 Jeremy Butler.
-	Telecommunication and Film Department, The University of Alabama.
-
-    This program is free software: you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation, either version 3 of the License, or
-    (at your option) any later version.
-
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
-
-    You should have received a copy of the GNU General Public License
-    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-*/
-
-// include configuration data for MySQL and Shot Logger login.
-
-require_once('../Connections/ShotLoggerVM.php'); 
-
-// Check to see if user is logged in.
-
-if (isset($_COOKIE['PrivatePageLogin'])) {
-   if ($_COOKIE['PrivatePageLogin'] == md5($password.$nonsense)) {
-
-// Insert content below.
+try {
+	//Open a connection to the database
+    require_once '../LaughLogger/Connections/llconnectPDO.php';
+	//The following two lines count the number of results in a manner that works across all database platforms.
+	$count = $db->query('SELECT COUNT(*) FROM sl_Title2');
+	$totalRows_rsSLTitle = $count->fetchColumn();
+	// IF conditional statement prevents the running of the next query if it's been found to be empty in the $count query.
+	if ($totalRows_rsSLTitle) {
+		// Connect to the database and run a query to return a record set (rs)
+		$sql = 'SELECT * FROM sl_Title2 LEFT JOIN `sl_ImdbMap` USING (IMDbID) ORDER BY slTitleID DESC';
+		$rsSLTitle = $db->query($sql);
+		// Capturing SQL errors
+		// Capture an array of errorInfo from the $db object
+		$errorInfo = $db->errorInfo();
+		// If errorInfo exists, put it in a variable. The THIRD bit of info in the error array [2] is the message.
+		if (isset($errorInfo[2])) {
+			$error = $errorInfo[2];
+		}
+	}
+// Catch PHP errors
+} catch (Exception $e) {
+    $error = $e->getMessage();
+}
 
 ?>
-
 <!-- BEGIN PASSWORD-PROTECTED CONTENT HERE -->
-
-<?php // require_once('../Connections/ShotLoggerVM.php'); ?>
-<?php
-if (!function_exists("GetSQLValueString")) {
-function GetSQLValueString($theValue, $theType, $theDefinedValue = "", $theNotDefinedValue = "") 
-{
-  if (PHP_VERSION < 6) {
-    $theValue = get_magic_quotes_gpc() ? stripslashes($theValue) : $theValue;
-  }
-
-  $theValue = function_exists("mysql_real_escape_string") ? mysql_real_escape_string($theValue) : mysql_escape_string($theValue);
-
-  switch ($theType) {
-    case "text":
-      $theValue = ($theValue != "") ? "'" . $theValue . "'" : "NULL";
-      break;    
-    case "long":
-    case "int":
-      $theValue = ($theValue != "") ? intval($theValue) : "NULL";
-      break;
-    case "double":
-      $theValue = ($theValue != "") ? doubleval($theValue) : "NULL";
-      break;
-    case "date":
-      $theValue = ($theValue != "") ? "'" . $theValue . "'" : "NULL";
-      break;
-    case "defined":
-      $theValue = ($theValue != "") ? $theDefinedValue : $theNotDefinedValue;
-      break;
-  }
-  return $theValue;
-}
-}
-
-mysql_select_db($database_ShotLoggerVM, $ShotLoggerVM);
-$query_rsSLTitle = "SELECT * FROM sl_Title2 ORDER BY slTitleID DESC";
-$rsSLTitle = mysql_query($query_rsSLTitle, $ShotLoggerVM) or die(mysql_error());
-$row_rsSLTitle = mysql_fetch_assoc($rsSLTitle);
-$totalRows_rsSLTitle = mysql_num_rows($rsSLTitle);
-
-?>
 <!doctype html>
 <html>
 <head>
 <meta charset="utf-8">
-<title>Shot Logger 2.1: Administration</title>
+<title>Shot Logger 2.2: Administration</title>
 <link href="admin.css" rel="stylesheet" type="text/css" />
 </head>
 
 <body>
 
-<?php 
-// Include function to convert BBCode to regular HTML
-// Includes CSS for converting BBCode to CSS
-// include_once("../../slgallery/shotlogger/includes/BBCodeToHTML.php");
-?>
-
-<h1 align="center">Shot Logger 2.1: Administration </h1>
+<h1 align="center">Shot Logger 2.2: Administration</h1>
 <table border="1" align="center">
   <tr>
     <td valign="top"><strong>Stats</strong></td>
   </tr>
   <tr>
     <td valign="top"><ul>
-      <li><a href="../images/indexAdminPW.php">View listing of uploaded files</a>, for importing data. After uploading a batch of files, <strong>be sure to update XML and downloadable files</strong>.</li>
-      <li><a href="../index.php">Go to Shot Logger Home</a></li>
+      <li><a href="../images/indexAdmin.php">View listing of uploaded files</a>, for importing data. After uploading a batch of files, <strong>be sure to update XML and downloadable files</strong>.</li>
+      <li><a href="../index.php">Go to Shot Logger v2 Home.</a></li>
       <li><strong>Update Data</strong>
         <ul>
-          <li><a href="datadownloadPW.php">Update downloadable files!</a></li>
-          <li><a href="SLStatsPW.php" target="_blank">Calculate cumulative stats!</a></li>
-          <li><em><strong>Manually</strong></em><strong> update the MySQL dump of the entire database (tcf-shotlogger.sql.gz)</strong></li>
+          <li><strong><a href="PHPgenerator/index.php" target="_blank">Modify Shot Logger data tables</a></strong> (PHPGenerator LITE--very limited data searching)</li>
+          <li><a href="">Update downloadable files!</a></li>
+          <li><a href="LLStats.php" target="_blank">Calculate cumulative stats!</a></li>
+          <li><em><strong>Manually</strong></em><strong> update the MySQL dump of the entire database</strong></li>
           </ul>
         </li>
     </ul></td>
@@ -112,28 +58,39 @@ $totalRows_rsSLTitle = mysql_num_rows($rsSLTitle);
 <table border="1" class="small" align="center">
   <thead>
   <tr>
-    <th width="50%" valign="top" nowrap="nowrap" scope="col">Shot Logger Titles -- Reverse Order<br />
-      (<a href="TitleList.php">detailed title list</a>)</th>
+    <th width="50%" valign="top" nowrap="nowrap" scope="col">Shot Logger Titles -- Reverse Order<br>
+<?php echo $totalRows_rsSLTitle . ' total records.' ; ?>
+</th>
     <th width="50%" valign="top" nowrap="nowrap" scope="col">Uploaded Image Directories<br />
-      (<a href="../images/indexAdminPW.php">details</a>)</th>
+      (<a href="../images/indexAdmin.php">details</a>)</th>
   </tr>
   </thead>
   <tr>
     <td width="50%" valign="top"><ul>
-      <?php do { 
-	  		$ID = $row_rsSLTitle['IMDbID'];
-			mysql_select_db($database_ShotLoggerVM, $ShotLoggerVM);
-			$query_rsIMDbTitle = "SELECT * FROM sl_ImdbMap WHERE IMDbID = '$ID' ";
-			$rsIMDbTitle = mysql_query($query_rsIMDbTitle, $ShotLoggerVM) or die(mysql_error());
-			$row_rsIMDbTitle = mysql_fetch_assoc($rsIMDbTitle);
-	  ?>
-      <li><a href="shotListPW.php?slTitleID=<?php echo $row_rsSLTitle['slTitleID']; ?>"><?php echo $row_rsSLTitle['slTitleID']; ?></a>: <?php if ($row_rsSLTitle['EpisodeDate']) { // Show if recordset not empty ?> <em><?php echo $row_rsIMDbTitle['ImdbTitle'] ?></em>: <?php } // Show if recordset not empty ?><?php echo $row_rsSLTitle['Title']; ?>
-  </li>
-      <?php } while ($row_rsSLTitle = mysql_fetch_assoc($rsSLTitle)); ?></ul></td>
+
+
+		<?php if (isset($error)) { // Check for SQL errors and display them
+    		echo "<h2>SQL error: " . $error . ".</h2><hr>";
+		}
+
+		while($row_rsSLTitle = $rsSLTitle->fetch()) { ; // WHILE loop
+		?>
+		<li>
+          <a href="FinalizeData.php?slTitleID=<?php echo $row_rsSLTitle['slTitleID']; ?>"><?php echo $row_rsSLTitle['slTitleID']; ?></a>: 
+          <em><?php echo $row_rsSLTitle['ImdbTitle'] ?></em>: 
+		  <?php echo $row_rsSLTitle['Title']; ?>
+		</li>
+		<?php } 
+		//while ($row_rsLLTitle = mysql_fetch_assoc($rsLLTitle)); ?>
+        </ul>
+
+	</td>
+
+
     <td width="50%" valign="top">
 	<ul>
 <?php     
-//	echo '<pre>';
+	// Get list of uploaded image directories
 	$dir =  '../images' ;
 	while($dirs = glob($dir . '/*', GLOB_ONLYDIR)) {
 		$dir .= '/*';
@@ -143,88 +100,20 @@ $totalRows_rsSLTitle = mysql_num_rows($rsSLTitle);
 			$d=array_merge($d,$dirs);
 		}
 	}
-//	print_r($d);
+	//	print_r($d);
 	natcasesort($d);
 	foreach($d as $file)  
 	{  
-//		echo "$file : filetype: " . filetype($file) . "<br />";  
 		echo "<li><strong>$file</strong> - " . date ("F d Y", filemtime($file)) . "</li>";
 	} 
-
-//	echo '</pre>';
 ?>    
 	</ul>
     </td>
   </tr>
 </table>
-<?php 
-include ('../includes/footerV2.php'); // Include the footer
-?>
-<?php
-mysql_free_result($rsIMDbTitle);
-mysql_free_result($rsSLTitle);
-mysql_free_result($rsAlbums);
-mysql_free_result($rsAlbumOfAlbums);
-?>
 
 <!-- END PASSWORD-PROTECTED CONTENT HERE -->
 
-<?php
-
-// Resume checking to see if user is logged in.
-
-   exit;
-   } else {
-      echo "Bad cookie.";
-      exit;
-   }
-}
-
-if (isset($_GET['p']) && $_GET['p'] == "login") {
-	echo "<!doctype html>
-		<html>
-		<head>
-		<meta charset=\"utf-8\">
-		<link href=\"../shotloggerV2_datatable.css\" rel=\"stylesheet\" type=\"text/css\" />
-		<title>Shot Logger: Login Problem</title>
-		<h1>Shot Logger: Login Problem</h1>";
-   if ($_POST['user'] != $username) {
-      echo "<strong>Sorry. I don't recognize that username.</strong><br />
-		<h2>Return to the previous page to try again.</h2>";
-      exit;
-   } else if ($_POST['keypass'] != $password) {
-      echo "<strong>Sorry. Invalid password.</strong>
-		<h2>Return to the previous page to try again.</h2>";
-      exit;
-   } else if ($_POST['user'] == $username && $_POST['keypass'] == $password) {
-      setcookie('PrivatePageLogin', md5($_POST['keypass'].$nonsense));
-      header("Location: $_SERVER[PHP_SELF]");
-   } else {
-      echo "<strong>Sorry. You could not be logged in at this time.</strong>";
-   }
-}
-?>
-<!doctype html>
-<html>
-<head>
-<meta charset="utf-8">
-<title>Shot Logger 2.1: Login</title>
-<link href="admin.css" rel="stylesheet" type="text/css" />
-</head>
-<body>
-<h1>Shot Logger: Administrator Login</h1>
-
-<!-- Begin password form -->
-
-<form action="<?php echo $_SERVER['PHP_SELF']; ?>?p=login" method="post">
-<label><strong>Username:</strong> <br />
-<input type="text" name="user" id="user" /></label><br />
-<label><strong>Password:</strong> <br />
-<input type="password" name="keypass" id="keypass" /></label>
-<p><input type="submit" id="submit" value="Login" /></p>
-</form>
-
-<!-- End password form -->
 
 <?php 
 include ('../includes/footerV2.php'); // Include the footer
